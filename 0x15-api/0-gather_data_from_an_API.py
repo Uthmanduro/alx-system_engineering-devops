38ec91cd781cb5a40219f45d379a5c8baef05918
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-04-24 13:08:33 -0700

    fixed bug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """script the uses REST API to return info about an employee TODO list"""
-import sys
 import requests
+import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         emp_id = int(sys.argv[1])
     except ValueError:
         exit()
-    user_todo = requests.get('https://jsonplaceholder.typicode.com/todos{}={}'.format("?userId", emp_id))
-    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(emp_id))
-    
+    user_todo = requests.get('https://jsonplaceholder.typicode.com/todos{}={}'.
+                             format("?userId", emp_id))
+    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                         format(emp_id))
+
     task_length = []
     count = []
     for todos in user_todo.json():
-        if todos.get("completed") == True:
+        if todos.get("completed") is True:
             count.append(todos.get("title"))
 
-    print("Employee {} is done with tasks({}/{}):".format(users.json().get("name"),
-          len(count), len(user_todo.json())))
-    [print("\t" + title) for title in count]
+    print("Employee {} is done with tasks({}/{}):".format(users.json().
+          get("name"), len(count), len(user_todo.json())))
+    [print("\t {}".format(title)) for title in count]
