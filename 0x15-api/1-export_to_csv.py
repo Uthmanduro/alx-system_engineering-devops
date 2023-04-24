7ec9ba24ec1cd6f753fec969db7f8e84fd6cecd0
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-04-24 15:01:21 -0700

    api to csv file

---
+++ 0x15-api/1-export_to_csv.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A python script that export data in csv format"""
+import csv
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    try:
+        emp_id = int(sys.argv[1])
+    except ValueError:
+        exit()
+    user_todo = requests.get('https://jsonplaceholder.typicode.com/todos{}={}'.
+                             format("?userId", emp_id))
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(emp_id))
+
+    filename = "{}.csv".format(emp_id)
+
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=",", quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for records in user_todo.json():
+            fmt = [emp_id, user.json().get("username"),
+                   records.get("completed"), records.get("title")]
+            writer.writerow(fmt)
