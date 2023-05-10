1cd1d4f2091f93643a7fe04d44ccb29ae0c748a7
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-05-10 08:04:44 -0700

    fix error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+#fix web stack 3 error
+exec { 'fix-error':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+debuggine web stack
