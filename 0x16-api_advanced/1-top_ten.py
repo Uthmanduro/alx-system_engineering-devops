e9a4677c879b52c59bfb2ebf705469b736a876ab
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-05-09 20:46:14 -0700

    fixed bug

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,12 +5,12 @@ import requests
 
 def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Badoo"}
+    headers = {"User-Agent": "ew Badoo"}
     res = requests.get(url, headers=headers, allow_redirects=False)
-    datas = res.json().get("data")
-    if not datas:
+    if res.status_code == 200:
+        datas = res.json().get("data").get("children")
+        if datas:
+            for items in datas:
+                print(items.get("data").get("title"))
+    else:
         print(None)
-    for items in datas.get("data").get("children"):
-        if items.get("data") and items.get("data").get("title"):
-            print(items.get("data").get("title"))
-    print(None)
